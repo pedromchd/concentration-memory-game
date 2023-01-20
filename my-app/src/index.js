ccc59da23ac6fe8aec39a8ae25674c160b3de3e6
pedromchd <pedrogmachado28@outlook.com>
2023-01-19 23:25:01 -0300

    game reboot

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,76 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function Card(props) {
   return (
-    <div className="wrapper" data-id="1">
-      <div className={`card ${props.isFlipped ? 'flipped' : ''}`} onClick={props.onClick}>
-        <div className="face front"></div>
-        <div className="face back"></div>
-      </div>
+    <div className="card">
+      <div className="face front"></div>
+      <div className="face back"></div>
     </div>
   );
 }
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: Array(24).fill(false),
-      flipped: 0,
-    };
-  }
-
-  handleClick(i) {
-    const flipped = this.state.flipped;
-    const cards = this.state.cards.slice();
-    if (cards[i]) {
-      return null;
-    }
-    cards[i] = true;
-    this.setState({
-      cards: cards,
-      flipped: flipped + 1,
-    });
-  }
-
-  renderCard(i) {
-    return <Card key={i} isFlipped={this.state.cards[i]} onClick={() => this.handleClick(i)} />;
-  }
-
-  checkCards() {
-    if (this.state.flipped < 2) {
-      return null;
-    }
-    const [card1, card2] = document.getElementsByClassName('flipped');
-    if (card1.dataset.id === card2.dataset.id) {
-      [card1, card2].forEach((c) => (c.style.animation = 'Success 500ms ease-in-out'));
-    } else {
-      [card1, card2].forEach((c) => (c.style.animation = 'Failure 500ms ease-in-out'));
-    }
-    // const cards = this.state.cards.slice();
-    // this.setState({
-    //   cards: cards.fill(false),
-    //   flipped: 0,
-    // });
-  }
-
-  componentDidMount() {
-    const board = document.querySelector('.board');
-    board.addEventListener('transitionend', () => this.checkCards());
-  }
-
-  render() {
-    return (
-      <div className="board">
-        {this.state.cards.map((value, index) => {
-          return this.renderCard(index);
-        })}
-      </div>
-    );
-  }
+function Board(props) {
+  return (
+    <div className="board">
+      <Card />
+    </div>
+  );
 }
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
+function Game(props) {
+  return <Board />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Game />);
