820a5045604a759b8a49f8b80938202b7fb5fcb0
pedromchd <pedrogmachado28@outlook.com>
2023-01-20 22:13:27 -0300

    flip da carta

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import cx from 'classnames';
 import './index.css';
 
 function Card(props) {
+  const [isFlipped, setFlipped] = useState(false);
+
   return (
-    <div className="card">
+    <div
+      className={cx('card', { flipped: isFlipped })}
+      onClick={() => setFlipped(!isFlipped)}
+    >
       <div className="face front"></div>
       <div className="face back"></div>
     </div>
@@ -14,6 +20,8 @@ function Card(props) {
 function Board(props) {
   return (
     <div className="board">
+      <Card />
+      <Card />
       <Card />
     </div>
   );
