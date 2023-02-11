7806ad9fc2bd675960c53544328c4147226f84d8
pedromchd <pedrogmachado28@outlook.com>
2023-02-11 10:53:17 -0300

    card flip handler

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import cx from 'classnames';
 import './index.scss';
 
-function Game(props) {
-  return;
+function Card(props: {}) {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div
+      className={cx('card', { toward: active })}
+      onClick={() => setActive(!active)}
+    >
+      <div className="face front"></div>
+      <div className="face back"></div>
+    </div>
+  );
+}
+
+function Board(props: {}) {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<Game />);
+root.render(<Board />);
